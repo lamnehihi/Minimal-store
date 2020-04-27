b843be69668833b705543d124a93986538147ed7
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-04-27 21:56:53 +0700

    create product and cart route

---
+++ controllers/product.controller.js
@@ -0,0 +1,3 @@
+module.exports.index = function(req, res) {
+  res.send('this is products page');
+}
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var productRoute = require('./routes/product.route');
+var cartRoute = require('./routes/cart.route');
+
+app.use('/product', productRoute);
+app.use('/cart', cartRoute);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
