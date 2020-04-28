97762f825c89af6024305b03ed11702993dcd482
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-04-28 10:21:34 +0700

    list all products to product page

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,10 +1,17 @@
-module.exports.index = function(req, res) {
+var Product = require('../models/product.model');
+
+module.exports.index = async function(req, res) {
   if(!req.signedCookies.cookiesId){
   res.cookie('cookiesId', '12345', {
     signed : true,
   });
   console.log('Set cookies!');
   }
+
+  var products = await Product.find();
+
   console.log(req.signedCookies);
-  res.render('products/index');
+  res.render('products/index', {
+    products : products
+  });
 }
